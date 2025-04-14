4161f4cd94ec83d08c7cf2a325255d480e8ec414
SATYASANA <satyabratasana4@gmail.com>
2025-04-14 23:33:56 +0530

    Updated app.js with latest changes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from "express"
 import morgan from "morgan";
-
+import cors from 'cors'
 import cookieParser from "cookie-parser";
 import userRouter from "./Router/user.route.js";
 import userTask from "./Router/task.route.js";
@@ -10,16 +10,14 @@ app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
-
+app.use(cors({
+  origin: "http://localhost:5173", 
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true,
+}));
 app.use('/api/v1',userRouter)
 app.use('/api/v1/task',userTask)
-app.get("/heavy", (req, res) => {
-    let count = 0;
-    for (let i = 0; i < 100000000000000000; i++) {
-      count += i;
-    }
-    res.send("Heavy calculation done 🧠");
-  });
+
   
 
 export default app;
