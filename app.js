54df2f5e02e4901d43199f715c40692101f6707d
SATYASANA <satyabratasana4@gmail.com>
2025-04-16 23:53:15 +0530

    modified the app

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://taskfroge-front.vercel.app/",
   credentials: true,
 }));
 app.use('/api/v1',userRouter)
