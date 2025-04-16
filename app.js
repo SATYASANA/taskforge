9bab2b622122dbb5d3d64d95912f97e8029d7ad8
SATYASANA <satyabratasana4@gmail.com>
2025-04-17 00:18:06 +0530

    another commit to change the frontentd url

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 app.use(cors({
-  origin: "https://taskfroge-front.vercel.app/",
+  origin: "https://taskinforge.netlify.app/",
   credentials: true,
 }));
 app.use('/api/v1',userRouter)
