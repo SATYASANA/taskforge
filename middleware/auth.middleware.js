5725e3a09411ab8ed596d7828bb5557dc9ca7013
SATYASANA <satyabratasana4@gmail.com>
2025-04-18 01:40:40 +0530

    another commit

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,37 +1,34 @@
-export const verifyToken = (req, res, next) => {
-    const token = req.cookies.token;
-    if (!token) {
+import jwt from 'jsonwebtoken'
+
+export const verifyToken = (req,res,next)=>{
+    const token = req.cookies.token
+
+    if(!token){
         return res.status(401).json({
-            success: false,
-            message: 'Access denied, no token provided'
-        });
+            success:false,
+            message:'Access denied, no token provided'
+        })
     }
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        console.log("Decoded token:", decoded); 
-        console.log("Decoded token:", decoded); // <--- DEBUG LOG
-        req.user = {
-            userId: decoded.userId,
-            role: decoded.role
-        };
-        next();
+        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+        req.user = decoded
+        next()
     } catch (error) {
-        return res.status(401).json({
-            success: false,
-            message: 'Invalid or expired token.'
-        });
+        return res.status(400).json({
+            success:false,
+            message:error.message
+        })
+        
     }
-};
+}
 
-export const authorizedRoles = (...allowedRoles) => {
-    return (req, res, next) => {
-        console.log("Allowed roles:", allowedRoles);         // <--- DEBUG LOG
-        console.log("User from token:", req.user);           // <--- DEBUG LOG
-        if (!req.user || !allowedRoles.includes(req.user.role)) {
-            return res.status(403).json({
-                message: 'Access denied you are not authorized'
-            });
-        }
-        next();
-    };
-};
+export const authorizedRoles=(...allowedRoles)=>{
+   return (req,res,next)=>{
+    if(!req.user || !allowedRoles.includes(req.user.role)){
+        return res.status(403).json({
+            message:'Access denied you are not authorized'
+        })
+    }
+    next()
+   }
+}
