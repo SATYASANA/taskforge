08afc927c18c15dff5a8afd1219048e317d473b5
SATYASANA <satyabratasana4@gmail.com>
2025-04-18 00:06:40 +0530

    remove verify token for ctesting

--- Router/task.route.js
+++ Router/task.route.js
@@ -8,5 +8,5 @@ const userTask = express.Router();
 userTask.post('/create-task',verifyToken,createTask)
 userTask.get('/active-task',verifyToken,activeTask)
 userTask.get('/today-task',verifyToken,todayTask)
-userTask.get('/all-task',verifyToken,allTask)
+userTask.get('/all-task',allTask)
 export default userTask
