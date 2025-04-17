4eaeedb905352aa25f729eda0f123348ff76780e
SATYASANA <satyabratasana4@gmail.com>
2025-04-18 01:13:00 +0530

    another commit

--- config/jwtToken.js
+++ config/jwtToken.js
@@ -26,7 +26,7 @@ export const generateJWTToken = (userId, role, res) => {
     secure: true,
     sameSite: 'None',
     maxAge: 24 * 60 * 60 * 1000, // 1 day
-    domain: 'taskinforge.netlify.app'
+   
   });
 
 };
